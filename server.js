a74a48e2acd6c1d27e2e5d5770ed8e26cc015459
Anusha Dunaboyina <anusha.dunaboyina@gmail.com>
2025-03-18 15:43:15 -0400

    Add logic to ensure uploads-documents folder is created automatically if missing

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import "dotenv/config";
 import multer from "multer";
 import path from "path";
 import { fileURLToPath } from "url";
+import fs from "fs";
 
 import documentRoutes from "./routes/documents-routes.js";
 
@@ -13,10 +14,17 @@ const PORT = process.env.PORT || 5050;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+// Ensure uploads-documents folder exists
+const uploadDir = path.join(__dirname, "/uploads-documents");
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir);
+  console.log("uploads-documents folder created successfully!");
+}
+
 // Middleware setup
 app.use(cors());
-app.use(express.json()); 
-app.use(express.urlencoded({ extended: true })); 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Serve static files from the uploads-documents directory
 app.use(
